56750d0a921d4d1ad642241dd4e100975432a10f
Pass1on_W <951050634@qq.com>
2026-03-14 00:30:36 +0800

    initial commit. 完成exercise 1。

--- ex1.c
+++ ex1.c
@@ -1,6 +1,13 @@
+#include <stdio.h>
+
 int main(int argc, char *argv[])
 {
     puts("Hello world.");
-
+puts("Hello world.\n");
+puts("Hello world.\n");
+puts("Hello world.\n");
+puts("Hello world.\n");
+puts("Hello world.\n");
+puts("Hello world.\n");
     return 0;
 }
